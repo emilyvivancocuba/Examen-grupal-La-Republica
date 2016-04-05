aaac45dd8cd4e4716140d218e6664b69bc656ef5
DESIDE ROMAN COLONIO <DESIDE ROMAN COLONIO>
2016-04-04 21:25:14 -0500

    iniciando a merge

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,14 +4,16 @@
 			  dots: true,
 			  infinite: false,
 			  speed: 300,
-			  slidesToShow: 4,
-			  slidesToScroll: 4,
+			  slidesToShow: 6,
+			  slidesToScroll: 6,
+			  prevArrow:false,
+			  nextArrow:false,
 			  responsive: [
 			    {
 			      breakpoint: 1024,
 			      settings: {
-			        slidesToShow: 3,
-			        slidesToScroll: 3,
+			        slidesToShow: 6,
+			        slidesToScroll: 6,
 			        infinite: true,
 			        dots: true
 			      }
